759508374a19e7debe908439910961c4d6d95c62
Apenas1Hugo <hugorodriguesm27@gmail.com>
2026-06-05 13:29:55 -0300

    tailwindcss e fontsource
    instalado tailwindcss e fontsource.

--- ReactAI/src/App.tsx
+++ ReactAI/src/App.tsx
@@ -1,11 +1,10 @@
-function App() {
-  
 
+function App() {
   return (
     <>
-      <h1>React AI  </h1>
+      <h1 className="text-4xl font-bold text-blue-500">React AI </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
