d8a8c3c2dffbf4e333ff937f4d9f776f8cf1fb7e
Apenas1Hugo <hugorodriguesm27@gmail.com>
2026-06-08 15:57:52 -0300

    Modularização do Formulário
    Formulário modularizado para que possa ser usado na criação de questionários.

---
+++ ReactAI/src/componentes/features/Simulation/Form.tsx
@@ -0,0 +1,15 @@
+import { simulationFormSteps } from '@/data/simulation'
+
+import { FormStep } from './FormStep'
+import { StepProgress } from './Progress'
+
+export const SimulationForm = () => {
+  const currentStep = simulationFormSteps[5]
+
+  return (
+    <>
+      <StepProgress currentStep={1} totalSteps={6} />
+      <FormStep key={currentStep.id} {...currentStep} />
+    </>
+  )
+}
