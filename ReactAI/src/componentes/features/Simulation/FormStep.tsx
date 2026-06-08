134a7de21bb9826f7e0ae097ffaf35965496d6cd
Apenas1Hugo <hugorodriguesm27@gmail.com>
2026-06-08 20:45:13 -0300

    Formulário completo
    Formulário está Funcionando perfeitamente e salvando valores no local storage

--- ReactAI/src/componentes/features/Simulation/FormStep.tsx
+++ ReactAI/src/componentes/features/Simulation/FormStep.tsx
@@ -2,6 +2,8 @@ import { ArrowLeft, ArrowRight, type LucideIcon } from 'lucide-react';
 
 import { Button } from '@/componentes/shared/Button';
 import { Input, type InputProps } from '@/componentes/shared/Input';
+import { type SyntheticEvent, useState } from 'react';
+import { formatCurrencyMask } from '@/utils/currency';
 
 export interface FormStepProps {
   id: string;
@@ -15,13 +17,34 @@ export interface FormStepProps {
   };
 }
 
+interface ActionsButtonsProps {
+  onBack: () => void;
+  onNext: (value: string) => void;
+  hideBackButton?: boolean;
+}
+
 export function FormStep({
   icon: Icon,
   title,
   question,
   inputProps,
   submitButtonProps,
-}: FormStepProps) {
+  hideBackButton,
+  onBack,
+  onNext,
+}: FormStepProps & ActionsButtonsProps) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!inputValue) {
+      return;
+    }
+
+    onNext(inputValue);
+  };
+
   return (
     <div className="bg-card rounded-2xl p-6 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)] sm:p-8">
       <div className="bg-primary h-15 w-15 mb-4 flex items-center justify-center rounded-xl">
@@ -31,18 +54,34 @@ export function FormStep({
       <h3 className="text-foreground mb-6 text-xl font-semibold leading-snug sm:text-2xl">
         {question}
       </h3>
-      <form className="flex flex-col gap-4">
-        <Input {...inputProps} />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <Input
+          {...inputProps}
+          value={inputValue}
+          onChange={(e) => {
+            const isCurrency = inputProps.prefix === 'R$';
+            const value = e.target.value;
+            setInputValue(isCurrency ? formatCurrencyMask(value) : value);
+          }}
+        />
         <div className="flex flex-col gap-3 sm:flex-row sm:gap-6">
+          {!hideBackButton && (
+            <Button
+              type="button"
+              onClick={onBack}
+              variant="ghost"
+              className="order-2 flex-1 justify-center rounded-xl py-3 sm:order-1"
+            >
+              <ArrowLeft size={16} />
+              Voltar
+            </Button>
+          )}
           <Button
-            type="button"
-            variant="ghost"
-            className="order-2 flex-1 justify-center rounded-xl py-3 sm:order-1"
+            type="submit"
+            variant="primary"
+            className="order-1 flex-1 sm:order-2"
+            disabled={!inputValue}
           >
-            <ArrowLeft size={16} />
-            Voltar
-          </Button>
-          <Button type="submit" variant="primary" className="order-1 flex-1 sm:order-2">
             {submitButtonProps?.label ?? 'Próximo'}
             {submitButtonProps?.emojiIcon ?? <ArrowRight size={16} />}
           </Button>
