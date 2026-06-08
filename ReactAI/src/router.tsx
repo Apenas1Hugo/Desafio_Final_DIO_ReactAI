ec4fb2fa15791a6d19b7c856f61cd1b09e19b877
Apenas1Hugo <hugorodriguesm27@gmail.com>
2026-06-08 11:50:22 -0300

    Header e mudança de tema
    Foi criado um header geral e foi criado o botão que permite alternar entre os temas.

--- ReactAI/src/router.tsx
+++ ReactAI/src/router.tsx
@@ -1,21 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom'
-
+import { RootLayout } from './componentes/layout/RootLayout'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: (
-      <>
-        <h1>Formulário de Simulação</h1>
-      </>
-    ),
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Formulário de Simulação</h1>,
+      },
+      {
+        path: '/resultado',
+        element: <h1>Resultado da Simulação</h1>,
+      },
+      {
+        path: '/historico',
+        element: <h1>Histórico de Simulações</h1>,
+      },
+    ],
   },
-  {
-    path: '/resultado',
-    element: <h1>Resultado de Simulação</h1>,
-  },
-  {
-    path: '/historico',
-    element: <h1>Histórico de Simulação</h1>,
-  },
-]);
+])
